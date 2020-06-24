b7dede9a2116918a23aa6d5a7b318b7fff888989
Anish.M.Prasad <anish.m.prasad@gmail.com>
2020-06-24 10:13:21 +0530

    code cleanup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,14 +13,16 @@
 					href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
 					target="_blank"
 					rel="noopener"
-				>babel</a>
+					>babel</a
+				>
 			</li>
 			<li>
 				<a
 					href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
 					target="_blank"
 					rel="noopener"
-				>eslint</a>
+					>eslint</a
+				>
 			</li>
 		</ul>
 		<h3>Essential Links</h3>
@@ -50,11 +52,9 @@
 				<a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
 			</li>
 			<li>
-				<a
-					href="https://github.com/vuejs/vue-devtools#vue-devtools"
-					target="_blank"
-					rel="noopener"
-				>vue-devtools</a>
+				<a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener"
+					>vue-devtools</a
+				>
 			</li>
 			<li>
 				<a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
--- src/pages/Counter/index.vue
+++ src/pages/Counter/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="appx">
-    Clicked: {{ $store.state.counter.count }} times, count is {{ evenOrOdd }}.
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
-    <button @click="incrementIfOdd">Increment if odd</button>
-    <button @click="incrementAsync">Increment async</button>
-  </div>
+	<div id="appx">
+		Clicked: {{ $store.state.counter.count }} times, count is {{ evenOrOdd }}.
+		<button @click="increment">+</button>
+		<button @click="decrement">-</button>
+		<button @click="incrementIfOdd">Increment if odd</button>
+		<button @click="incrementAsync">Increment async</button>
+	</div>
 </template>
 
 <script src="./index.js" />
